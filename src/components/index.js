aeb18e4a814ab2db6c1d1ee73eaac9286e576a14
BBAK_JUN <wnsguddl789@gmail.com>
2025-07-06 20:24:36 +0900

    fix(components): update component export system
    - ensure Toast component is properly exported
    - maintain consistent component import structure
    - fix missing component exports

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./header";
+export { default as Footer } from "./footer";
+export { default as CartModal } from "./cart-modal";
+export { default as Toast } from "./toast";
