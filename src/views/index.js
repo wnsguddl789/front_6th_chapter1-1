aa92e560d1ab7c0d84ce968c1542ebd5cad015bf
BBAK_JUN <wnsguddl789@gmail.com>
2025-07-06 23:55:00 +0900

    rename: update not-found page path to 404
    - change NotFoundPageView import path from './not-found/view' to './404/view'
    - align with conventional 404 page naming convention

--- src/views/index.js
+++ src/views/index.js
@@ -1,2 +1,3 @@
 export { default as HomePageView } from "./home/view";
 export { default as NotFoundPageView } from "./404/view";
+export { default as ProductDetailPageView } from "./product-detail/view";
