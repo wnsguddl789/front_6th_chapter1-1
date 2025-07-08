3c9c5f028c0c76989a9350c8aa6a68866fefd1f0
BBAK_JUN <wnsguddl789@gmail.com>
2025-07-09 01:54:32 +0900

    feat(views): improve 404 view and view index
    - enhance 404 error page functionality
    - update view index configuration

--- src/views/404/view.js
+++ src/views/404/view.js
@@ -71,7 +71,7 @@ export default class NotFoundPage extends BaseComponent {
                 <rect x="130" y="130" width="60" height="2" rx="1" fill="url(#blueGradient)" opacity="0.3"/>
             </svg>
         
-        <a href="/" data-link class="inline-block px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">홈으로</a>
+        <a href="/" data-link="" class="inline-block px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">홈으로</a>
         </div>
     </main>`;
   }
--- src/views/index.js
+++ src/views/index.js
@@ -1,3 +1,8 @@
+// view
 export { default as HomePageView } from "./home/view";
 export { default as NotFoundPageView } from "./404/view";
 export { default as ProductDetailPageView } from "./product-detail/view";
+
+// header
+export { default as ProductListHeaderComponent } from "./home/header";
+export { default as ProductDetailHeaderComponent } from "./product-detail/header";
